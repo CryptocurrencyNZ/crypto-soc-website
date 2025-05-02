9c02b60d68ac87b9ef35c96255f3838662658a46
ItsBreaker <jayden6297@gmail.com>
2025-05-02 17:06:47 +1200

    init

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import Sponsor from "./sponsors";
+import Awards from "./Awards";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <Sponsor />
+      <Awards />
+
+      {/*<ExpoSection />*/}
+    </>
+  );
+};
+
+export default HomePage;
